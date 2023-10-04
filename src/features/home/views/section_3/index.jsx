32e26a6745bc2e8acd8a46d79d11afddc38a2da1
richi-coder <richicoder@gmail.com>
2023-10-04 13:45:10 -0400

    laying out

--- src/features/home/views/section_3/index.jsx
+++ src/features/home/views/section_3/index.jsx
@@ -1,10 +1,43 @@
-import styles from './styles.module.css'
+import styles from "./styles.module.css";
+// import rocket from '@/assets/images/about-us/rocket.webp';
 
-const SectionThree = ({t}) =>{
-    return(
-        <div className={styles.container}>
-            Section 3
+const SectionThree = ({ t }) => {
+  return (
+    <div className={styles.container}>
+
+      <div className={styles.header}>
+        <h3>{t("meetUs.whoWeAreTitle")}</h3>
+        <p>{t("meetUs.largePortfolio")}</p>
+      </div>
+
+      <div className={styles.cardsWrapper}>
+
+        <div className={styles.card}>
+          <div className={styles.cardTitle}>
+            {/* <img src={rocket} alt="rocket_mon-basa" /> */}
+            <h4>{t("meetUs.mision.title")}</h4>
+          </div>
+          <p className={styles.cardContent}>{t("meetUs.mision.content")}</p>
         </div>
-    )
-}
-export default SectionThree
+
+        <div className={styles.card}>
+          <div className={styles.cardTitle}>
+            {/* <img src={rocket} alt="rocket_mon-basa" /> */}
+            <h4>{t("meetUs.vision.title")}</h4>
+          </div>
+          <p className={styles.cardContent}>{t("meetUs.vision.content")}</p>
+        </div>
+
+        <div className={styles.card}>
+          <div className={styles.cardTitle}>
+            {/* <img src={rocket} alt="rocket_mon-basa" /> */}
+            <h4>{t("meetUs.values.title")}</h4>
+          </div>
+          <p className={styles.cardContent}>{t("meetUs.values.content")}</p>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+export default SectionThree;
